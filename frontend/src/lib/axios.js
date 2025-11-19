8464f6a9191153747b2760f6d2b23c5356786c58
assal <assal@gmail.com>
2025-11-19 08:56:02 +0200

    ///

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "/api",
   withCredentials: true, // send cookies to the server
 });
 
